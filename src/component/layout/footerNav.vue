32210d64821ecbfd6db714a9c55137fc5e5d687d
xiajing7478 <260594736@qq.com>
2017-03-06 15:32:27 +0800

    add icons

--- src/component/layout/footerNav.vue
+++ src/component/layout/footerNav.vue
@@ -1,8 +1,14 @@
 <template>
     <div class="footer pure-g m_center">
-        <div @click='getUrl' class="pure-u-1-3"><router-link class='active' to="/">电影</router-link></div>
-        <div @click='getUrl' class="pure-u-1-3"><router-link to="/register">影院</router-link></div>
-        <div @click='getUrl' class="pure-u-1-3"><router-link to="/user">我的</router-link></div>
+        <div @click='getUrl' class="pure-u-1-3">
+            <router-link class='active' to="/">电影</router-link>
+        </div>
+        <div @click='getUrl' class="pure-u-1-3">
+            <router-link to="/register">影院</router-link>
+        </div>
+        <div @click='getUrl' class="pure-u-1-3">
+            <router-link to="/user">我的</router-link>
+        </div>
     </div>
 </template>
 <script>
