ce86363bdea7c7ffd92c2ba6c236f18bd83b3039
xiajing7478 <260594736@qq.com>
2017-03-06 10:18:12 +0800

    update

--- src/component/layout/footerNav.vue
+++ src/component/layout/footerNav.vue
@@ -2,7 +2,7 @@
     <div class="footer pure-g m_center">
         <div @click='getUrl' class="pure-u-1-3 active"><router-link to="/">电影</router-link></div>
         <div @click='getUrl' class="pure-u-1-3"><router-link to="/register">影院</router-link></div>
-        <div @click='getUrl' class="pure-u-1-3"><router-link to="/login">我的</router-link></div>
+        <div @click='getUrl' class="pure-u-1-3"><router-link to="/user">我的</router-link></div>
     </div>
 </template>
 <script>
